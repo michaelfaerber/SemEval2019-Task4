98633f19f547698044ac4c4c93938351620de869
agonqurdina <agonqurdina@gmail.com>
2018-12-14 22:45:30 +0100

    change the model arch && refactor the code

--- train_conv_model.py
+++ train_conv_model.py
@@ -1,6 +1,6 @@
 from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Conv1D, MaxPooling1D, Activation, Embedding, Flatten
+from keras.layers import Dense, Dropout, Conv1D, MaxPooling1D, Activation, Embedding, Flatten, GlobalMaxPooling1D
 from keras.engine.input_layer import Input
 import argparse
 import clean_shuffle
@@ -13,8 +13,6 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 sem_eval_path = ''
-dataset_type = ''
-
 
 def build_pv_models(df, sem_eval_path):
     """ Function which builds the paragraph vector models (title and content models) based on the 
@@ -38,35 +36,38 @@ def build_pv_models(df, sem_eval_path):
         pickle.dump(pv, pfile, pickle.HIGHEST_PROTOCOL)
     return pv
 
-def generate_data():
+def get_data():
     pv_location = os.path.join(sem_eval_path, 'models', 'pv_object.pickle')
-    with open(pv_location, 'rb') as pfile:
-        print("Loading paragraph vector instance from pickle...")
-        pv = pickle.load(pfile)
+    try:
+        # If a paragraph vector has already been pickled, load it in.
+        with open(pv_location, 'rb') as pfile:
+            print("Loading paragraph vector instance from pickle...")
+            pv = pickle.load(pfile)
+    except FileNotFoundError:
+        filename = os.path.join(sem_eval_path, 'data', 'IntegratedFiles', 'buzzfeed_training_withid.tsv')
+        df_location = os.path.join(sem_eval_path, 'data', 'Pickles', 'training_df.pickle')
+        # Doc2Vec training required
+        df = clean_shuffle.read_prepare_df(filename, file_path=df_location)
+        import sys
+        sys.exit()
+        print("Training paragraph vectors...")
+        pv = build_pv_models(df, sem_eval_path)
 
-    print("Getting vector label mapping...")
     X_train, y_train_df = get_vector_label_mapping(pv, 'concat')
 
     # Needed to reshape from (n, 300) to (n, 300, 1) to serve as an input to the Conv layer
     X_train = numpy.atleast_3d(X_train)
-    print('df reshaped')
 
-    return (X_train, y_train_df.hyperpartisan)
+    return X_train, y_train_df.hyperpartisan
 
 def main():
-    batch_size = 32 # default
-    filters = 250
-    kernel_size = 5
-    hidden_dims = 500
-    epochs = 10
-
     parser = argparse.ArgumentParser()
     parser.add_argument("--path",'-p', default="/home/agon/Files/SemEval",
                         help="Use this argument to change the SemEval directory path (the default path is: '/home/ashwath/Files/SemEval')")
     parser.add_argument("--epochs", '-e', default="10",
                         help="Use this argument to set the number of epochs. Default: 10")
-    parser.add_argument("--filters", '-f', default="250",
-                        help="Use this argument to set the number of filters. Default: 100")
+    parser.add_argument("--filters", '-f', default="64",
+                        help="Use this argument to set the number of filters. Default: 64")
     parser.add_argument("--kernel", '-k', default="4",
                         help="Use this argument to set the size of kernels. Default: 4")
     args = parser.parse_args()
@@ -74,12 +75,17 @@ def main():
     global sem_eval_path
     sem_eval_path = args.path
 
+    # Hyperparameters
     filters = int(args.filters)
     kernel_size = int(args.kernel)
     epochs = int(args.epochs)
+    hidden_dims = 250
+    batch_size = 32 # default
 
-    X_train, y_train = generate_data()
+    # Get data
+    X_train, y_train = get_data()
 
+    # Model definition
     model = Sequential()
 
     model.add(Conv1D(filters,
@@ -87,9 +93,16 @@ def main():
                     activation='relu',
                     input_shape=X_train[0].shape))
     model.add(Dropout(0.2))
-    model.add(MaxPooling1D(pool_size=2))
+    model.add(MaxPooling1D(pool_size=4))
 
-    model.add(Flatten())
+    model.add(Conv1D(filters,
+                    kernel_size,
+                    activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(MaxPooling1D(pool_size=4))
+
+    model.add(GlobalMaxPooling1D())
+    # model.add(Flatten())
 
     model.add(Dense(hidden_dims, activation='relu'))
     model.add(Dropout(0.5))
